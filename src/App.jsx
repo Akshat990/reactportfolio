e4b6bcdd8e7a30a7cb1815dbdd8a6e2b596215f1
Akshat <raghuvanshiakshatsingh@gmail.com>
2023-06-03 02:15:53 +0530

    :fire:portfolio complete v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
-const App = () => {
-  return <div>Hello world!</div>;
-};
+import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro"
+import Portfolio from "./components/portfolio/Portfolio"
+import Works from "./components/works/works"
+
+import Contact from "./components/Contact/Contact"
+import "./app.scss"
+import { useState } from "react";
+import Menu from "./components/menu/Menu";
+
+function App() {
+  const [menuOpen,setMenuOpen] = useState(false)
+  return (
+    <div className="app">
+     <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+     <Menu  menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+     <div className="sections">
+      <Intro/>
+      <Portfolio/>
+      <Works/>
+      <Contact/>
+     </div>
+     
+    </div>
+  );
+}
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,45 @@
+export const featuredPortfolio = [
+    {
+        id: 1,
+        title: "Movie Library",
+        img: "../../assets/Portfolio_pics/s1.png",
+    },
+    {
+        id: 2,
+        title: "Weather App",
+        img: "../../assets/Portfolio_pics/s2.png",
+    },
+    {
+        id: 3,
+        title: "Text Editor",
+        img: "../../assets/Portfolio_pics/s3.png",
+    },
+    {
+        id: 4,
+        title: "AI Face Detection",
+        img: "../../assets/Portfolio_pics/fd.png",
+    },
+];
+
+export const webPortfolio = [
+    {
+        id: 1,
+        title: "Weather App",
+        img: "../../assets/Portfolio_pics/s2.png",
+    },
+    {
+        id: 2,
+        title: "Movie Library",
+        img: "../../assets/Portfolio_pics/s1.png",
+    },
+    {
+        id: 3,
+        title: "AI Face Detection",
+        img: "../../assets/Portfolio_pics/fd.png",
+    },
+    {
+        id: 4,
+        title: "Text Editor",
+        img: "../../assets/Portfolio_pics/s3.png",
+    },
+];
